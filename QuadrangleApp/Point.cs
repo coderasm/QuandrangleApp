399543cb15983a1e3df44cfa465789a5908c6910
pkrueger <philip.krueger@caci.com>
2019-04-25 15:43:16 -0700

    first push

---
+++ QuadrangleApp/Point.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace QuadrangleApp
+{
+  class Point
+  {
+    private double x = 0;
+    private double y = 0;
+    private Point(double x, double y)
+    {
+      this.x = x;
+      this.y = y;
+    }
+
+    public Point create(double x, double y)
+    {
+      return new Point(x, y);
+    }
+
+    public void setPoint(double x, double y)
+    {
+      this.x = x;
+      this.y = y;
+    }
+
+    public double dist(Point p)  // compute the distance of point p to the current point
+    {
+      double distance;
+      distance = Math.Sqrt((x - p.x) * (x - p.x) + (y - p.y) * (y - p.y));
+      return distance;
+    }
+    public void print()
+    {
+      Console.Write("Point: ({0}, {1})", x, y);
+    }
+  }
+}
---
+++ QuadrangleApp/Quadrangle.cs
@@ -0,0 +1,37 @@
+﻿namespace QuadrangleApp
+{
+  class Quadrangle
+  {
+    private Point[] vertices = { };
+    private Quadrangle(Point[] vertices)
+    {
+      if (vertices.Length == 4)
+        this.vertices = vertices;
+    }
+
+    public Quadrangle create(Point[] vertices)
+    {
+      return new Quadrangle(vertices);
+    }
+
+    public bool isTrapezoid()
+    {
+
+    }
+
+    public bool isRectangle()
+    {
+
+    }
+
+    public bool isSquare()
+    {
+
+    }
+
+    public bool isParallelogram()
+    {
+
+    }
+  }
+}
